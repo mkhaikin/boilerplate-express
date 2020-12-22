3b9a9670c27818443cda02eced485f4b0e2e7a36
Michael Khaikin <michael.khaikin@gmail.com>
2020-12-22 21:32:53 +0000

    static css and file can now be served

--- myApp.js
+++ myApp.js
@@ -3,24 +3,14 @@ const app = express();
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/, (req,res) => {
+// Serve a simple html file
+app.get('/', (req,res) => {
 	res.sendFile( __dirname + '/views/index.html')
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/json', (req,res) => {
+	res.json({"message": "Hello json"});
+});
 
 
 
