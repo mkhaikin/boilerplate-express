e7f2fbd522332a41825766ade9fdd94903bac801
Michael Khaikin <michael.khaikin@gmail.com>
2020-12-23 02:08:58 +0000

    logger middleware

--- myApp.js
+++ myApp.js
@@ -2,19 +2,26 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/public'))
-
+// Logger
+app.use('*', (req,res,next) => {
+	console.log(req.method +" "+req.path+" - "+req.ip);
+	next();
+})
 // Serve a simple html file
 app.get('/', (req,res) => {
 	res.sendFile( __dirname + '/views/index.html')
 });
 
 app.get('/json', (req,res) => {
-	if(process.env.MESSAGE_STYLE==="uppercase"){
-		res.json({"message": "HELLO JSON"});
-	} else {
-			res.json({"message": "Hello json"});
-	}
+	res.json({"message": "Hello json"});
 });
+// app.get('/json', (req,res) => {
+// 	if(process.env.MESSAGE_STYLE==="uppercase"){
+// 		res.json({"message": "HELLO JSON"});
+// 	} else {
+// 			res.json({"message": "Hello json"});
+// 	}
+// });
 
 
 
